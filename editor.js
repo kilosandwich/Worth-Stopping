3f73ede8245b846f95ac20216136e960644d5c49
Phillip Wandyez <wandyez@gmail.com>
2025-12-21 21:29:44 -0500

    Add files via upload

--- editor.js
+++ editor.js
@@ -157,6 +157,22 @@ async function generateUID(geojsonFilePath = "locations.geojson") {
   }
 }
 
+function autofillEditorInput(uid,props ="",coordinates){
+  //make sure to convert the coordinates into a usable type
+  document.getElementById("editor-uid").value = props.uid || "";
+  document.getElementById("editor-name").value = props.name || "";
+  document.getElementById("editor-street").value = props.street|| "";
+  document.getElementById("editor-country").value = props.country || "";
+  document.getElementById("editor-state").value = props.state || "";
+  document.getElementById("editor-county").value = props.county || "";
+  document.getElementById("editor-city").value = props.city || "";
+  document.getElementById("editor-website").value = props.website || "";
+  document.getElementById("editor-coordinates").value = coordinates || "";
+  document.getElementById("editor-description").value = props.description || "";
+  //you will need to figure out how to autofill the tags section considering there are literally ENDLESS tags to add
+  //and you might want to make them up on the fly.
+}
+
 //define a function that retrieves the data from the given UID of the geoJSON, it needs to return all relevant features to the creation of
 //a location, including populating a tag array
 
@@ -166,15 +182,16 @@ async function generateUID(geojsonFilePath = "locations.geojson") {
 async function editor(uid = "", geojsonFilePath = "locations.geojson"){
     console.log("The editor has been called for location: ");
     console.log(uid);
-    compareUID = await compareUID(uid, geojsonFilePath)
+    UIDExists = await compareUID(uid, geojsonFilePath)
 
     //if no UID exists (either none given, or not present), generate one for the new location. 
-    if ((uid === "") || (!compareUID)) {
+    if ((uid === "") || (!UIDExists)) {
         //the UID clearly does not exist, or isn't present
         //so generate one
         uid = await generateUID(geojsonFilePath);
         console.log("testing generation of UID");
         console.log(uid);
+        //make sure to populate the UID now
     }else{
         //since the UID exists, gather the information from the existing uid. 
         //it is easier to gather all the coordinates THEN parse them
@@ -185,7 +202,11 @@ async function editor(uid = "", geojsonFilePath = "locations.geojson"){
          console.log("Here are the properties and coordinates for the given UID:")
          console.log(props);
          console.log(coordinates);
+         autofillEditorInput(uid,props,coordinates);
+
+         //time to populate the editor window with all the properties we have gathered. Yay.
     }
+    
     //Great, now that we have the properies of the given UID (if they existed in the first place)
     //It is time to fill in the editor menu (which needs to persist if it is accidentally closed!)
     //The editor should probably exist as a div that is constantly hidden in the HTML. I mean that makes sense right?
@@ -196,7 +217,8 @@ async function editor(uid = "", geojsonFilePath = "locations.geojson"){
     //OK, the local geoJSON file loaded successfull (maybe do a performance check where the data within it)
     //is compared to the data in the forms
     //if successfully compared (what we want to submit (ONLY ONE LOCATION)) then edit the main geoJSON file. 
-
+    console.log("I am attempting to open the editor");
+    openLocationEditor();
     
 }
 
@@ -225,4 +247,175 @@ function appendAndReplace(tempgeojsonFilePath, geojsonFilePath){
 //and then fetches the latitude and longitude 
 async function getLatLong(){
 
+}
+
+
+function openLocationEditor() {
+document.getElementById("edit-dropdown").style.display = "block";
+
+}
+
+function closeLocationEditor() {
+  document.getElementById("edit-dropdown").style.display = "none";
+}
+
+document.getElementById("add-location")
+  ?.addEventListener("click", () => {
+    console.log("Add location button clicked!");
+    editor();
+  });
+//button function in order to open a google maps location of the editor
+document.getElementById("editor-open-google-maps")
+  ?.addEventListener("click", () => {
+    lat = editorGetLat();
+    lng = editorGetLang();
+    openGoogleMapsToCoordinates(lat,lng);
+
+  });
+
+function openGoogleMapsToCoordinates(lat,lng){
+    const url =
+      `https://www.google.com/maps/search/?api=1` +
+      `&query=${lat},${lng}`;
+
+    window.open(url, "_blank", "noopener");
+}
+
+
+//helper functions to get the various 
+//things from their various form locations
+//from the index.html editor
+function editorGetName(){
+  const nameInput = document.getElementById("editor-name");
+  if (!nameInput){
+    console.log("editor-name input not found!");
+    return "";
+  }
+
+  //trim the finale value or else it will be whitespacey
+  return nameInput.value.trim();
+}
+
+function editorGetCoordinates() {
+    const coordinatesInput = document.getElementById("editor-coordinates");
+
+    if (!coordinatesInput) {
+        console.warn("editor-coordinates input not found!");
+        return null;
+    }
+
+    const raw = coordinatesInput.value.trim();
+
+    if (!raw) {
+        console.warn("No coordinates entered");
+        return null;
+    }
+
+    // Expect: "lat,lng" or "lat, lng"
+    const parts = raw.split(",").map(p => p.trim());
+
+    if (parts.length !== 2) {
+        console.warn("Invalid coordinate format (expected 'lat, lng'):", raw);
+        return null;
+    }
+
+      // -------------------------------
+      // 3. TRANSFORM TO NUMBERS (KEY STEP)
+      // -------------------------------
+      const lat = Number(parts[0]);
+      const lng = Number(parts[1]);
+
+      if (!Number.isFinite(lat) || !Number.isFinite(lng)) {
+          console.warn("Coordinates are not valid numbers:", parts);
+          return null;
+      }
+
+      // -------------------------------
+      // 4. (Optional but recommended)
+      //    Validate geographic bounds
+      // -------------------------------
+      if (lat < -90 || lat > 90 || lng < -180 || lng > 180) {
+          console.warn("Coordinates out of bounds:", { lat, lng });
+          return null;
+      }
+
+    console.log("Coordinates retrieved:", { lat, lng });
+
+    return { lat, lng };
+}
+//helped functions that do what the getCoordinates function does but just returns a single number. WAY easier to use. 
+function editorGetLat() {
+    const coords = editorGetCoordinates();
+    return coords ? coords.lat : null;
+}
+
+function editorGetLang() {
+    const coords = editorGetCoordinates();
+    return coords ? coords.lng : null;
+}
+
+function editorGetStreetAddress(){
+  const streetAdressInput = document.getElementById("editor-street");
+  if (!streetAdressInput){
+    console.log("editor-street input not found!");
+    return "";
+  }
+
+  //trim the finale value or else it will be whitespacey
+  return streetAdressInput.value.trim();
+}
+
+function editorGetState(){
+  const stateInput = document.getElementById("editor-state");
+  if (!stateInput){
+    console.log("editor-state input not found!");
+    return "";
+  }
+
+  //trim the finale value or else it will be whitespacey
+  return stateInput.value.trim();
+}
+
+function editorGetCounty(){
+  const countyInput = document.getElementById("editor-county");
+  if (!countyInput){
+    console.log("editor-county input not found!");
+    return "";
+  }
+
+  //trim the finale value or else it will be whitespacey
+  return countyInput.value.trim();
+}
+
+function editorGetCity(){
+  const cityInput = document.getElementById("editor-city");
+  if (!cityInput){
+    console.log("editor-city input not found!");
+    return "";
+  }
+
+  //trim the finale value or else it will be whitespacey
+  return cityInput.value.trim();
+}
+
+function editorGetWebsite(){
+  const websiteInput = document.getElementById("editor-website");
+  if (!websiteInput){
+    console.log("editor-website input not found!");
+    return "";
+  }
+
+  //trim the finale value or else it will be whitespacey
+  return websiteInput.value.trim();
+}
+
+function editorGetDescription(){
+  const descriptionInput = document.getElementById("editor-description");
+  if (!descriptionInput){
+    console.log("editor-description input not found!");
+    return "";
+  }
+
+  //trim the finale value or else it will be whitespacey
+  return descriptionInput.value.trim();
 }
